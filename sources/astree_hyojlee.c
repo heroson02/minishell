87d7782994fa913146074ad226eb5b4370a9c397
Hyojeong Lee <hyojlee@c5r1s7.42seoul.kr>
2022-04-20 19:37:15 +0900

    22.04.20 19:37 AST 구현 수정 ver.0.1 by hyojlee 리다이렉션의 처리 순서를 생각해봐야함.

--- sources/astree_hyojlee.c
+++ sources/astree_hyojlee.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   astree copy.c                                      :+:      :+:    :+:   */
+/*   astree_hyojlee.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyojlee <hyojlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 19:17:26 by hyojlee           #+#    #+#             */
-/*   Updated: 2022/04/20 16:58:53 by hyojlee          ###   ########.fr       */
+/*   Updated: 2022/04/20 19:31:57 by hyojlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,10 +54,18 @@ int	insert_redir(t_astree *tree, t_node *node)
 		return (FALSE);
 	cur = tree->root;
 	pre = cur;
-	while (cur && (cur->type == PIPE || cur->type == REDIR))
+	while (cur && cur->type == PIPE)
 	{
 		pre = cur;
 		cur = cur->right;
+	}
+	if (node->data[0] == '<') // < 들이 계산되고 > 가 계산되어야 함 => <가 나오면 무조건 밑으로 보냄
+	{
+		while (cur && cur->type == REDIR && cur->data[0] != '<')
+		{
+			pre = cur;
+			cur = cur->right;
+		}
 	}
 	node->left = cur;
 	if (pre == cur)
@@ -72,25 +80,27 @@ int	insert_token(t_astree *tree, t_node *node)
 	t_node	*cur;
 	t_node	*pre;
 
-	if (!tree || !(tree->root))
+	if (!tree)
 		return (FALSE);
 	cur = tree->root;
 	pre = cur;
-	while (cur->right)
-		cur = cur->right;
-	if (cur->type == PIPE || cur->type == REDIR)
-	{
-		cur->right = node;
-		return (TRUE);
-	}
-	while (cur)
+	while (cur && (cur->type == PIPE || cur->type == REDIR))
 	{
 		pre = cur;
-		cur = cur->left;
+		cur = cur->right;
 	}
-	if (pre == tree->root)
+	if (pre == cur)
 		tree->root = node;
+	else if (!cur && (pre->type == PIPE || pre->type == REDIR))
+		pre->right = node;
 	else
+	{
+		while (cur)
+		{
+			pre = cur;
+			cur = cur->left;
+		}
 		pre->left = node;
+	}
 	return (TRUE);
 }
