da188adf3d453cc59fe266f7d78f973ccf15eab8
heroson02 <heroson0925@gmail.com>
2022-05-04 00:26:28 +0900

    redir 오류 수정 완료
    추가적인 테스트가 필요하다.

--- sources/redir.c
+++ sources/redir.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redir.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyojlee <hyojlee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: yson <yson@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 14:50:14 by hyojlee           #+#    #+#             */
-/*   Updated: 2022/05/03 18:38:06 by hyojlee          ###   ########.fr       */
+/*   Updated: 2022/05/04 00:24:15 by yson             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,13 @@ int	disconnect_redir(t_info *info)
 	status = 0;
 	if (info->file->open_stdin > 0)
 	{
+		close(info->file->open_stdin);
 		if (dup2(info->file->origin_stdin, STDIN) < 0)
 			return (FALSE);
 	}
 	if (info->file->open_stdout > 0)
 	{
+		close(info->file->open_stdout);
 		if (dup2(info->file->origin_stdout, STDOUT) < 0)
 			return (FALSE);
 	}
@@ -55,11 +57,9 @@ static void	init_redir(t_info *info)
 
 int	redirection(t_info *info, t_node *node)
 {
-	int		fd;
 	char	*path;
 
 	path = node->right->data;
-	//기존에 redir을 통해 fd가 존재한다면 close 해주기
 	init_redir(info);
 	if (!ft_strcmp(node->data, ">"))
 		info->file->open_stdout = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -69,7 +69,7 @@ int	redirection(t_info *info, t_node *node)
 		info->file->open_stdin = open(path, O_RDONLY, 0644);
 	else if (!ft_strcmp(node->data, "<<"))
 		// heredoc
-	if (fd < 0)
-		printf("minishell: %s: No such file or directory\n", path);
+	if (info->file->open_stdout < 0 || info->file->open_stdin < 0)
+		return (printf("minishell: %s: No such file or directory\n", path));
 	read_tree(info, node->left);
 }
