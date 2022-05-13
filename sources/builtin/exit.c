847f30d74baab7358562d49af34c25270e58afbe
Hyojeong Lee <hyojlee@c6r4s7.42seoul.kr>
2022-05-13 21:24:51 +0900

    22.05.13 21:24 exit 출력 리다이렉션 만났을 때 파일에 exit 문자열이 써지지 않고 터미널에 출력되어야 한다. 따라서 ft_putendl_fd(exit, get_info()->file->origin_stdout);으로 해당 문자열을 출력함.

--- sources/builtin/exit.c
+++ sources/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: hyojlee <hyojlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 14:50:30 by hyojlee           #+#    #+#             */
-/*   Updated: 2022/05/13 20:35:59 by hyojlee          ###   ########.fr       */
+/*   Updated: 2022/05/13 21:23:13 by hyojlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ void	builtin_exit(t_node *cmd)
 {
 
 	get_info()->exitcode = 0;
-	ft_putendl_fd("exit", STDOUT);
+	ft_putendl_fd("exit", get_info()->file->origin_stdout);
 	if (cmd->left)
 	{
 		get_info()->exitcode = ft_atoi_(cmd->left->data);
